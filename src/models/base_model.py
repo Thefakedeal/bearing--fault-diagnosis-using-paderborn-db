5abbb99fbcfe5988cae542e19ee8dee63d2a713d
Samir Shrestha <theonlysamir@gmail.com>
2026-02-15 21:42:44 -0500

    fix abstract method

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -2,22 +2,17 @@
 
 class BaseModel:
     
-    @abstractmethod
     def train(self, X, y):
         pass
 
-    @abstractmethod
     def predict(self, X):
         pass
 
-    @abstractmethod
     def save(self, path):
         pass
 
-    @abstractmethod
     def load(self, path):
         pass
 
-    @abstractmethod
     def export(self, path):
         pass
