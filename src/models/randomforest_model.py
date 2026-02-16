da9a11a207b81c917eb4346ca918f57373f04a43
Samir Shrestha <theonlysamir@gmail.com>
2026-02-15 22:00:49 -0500

    clean up

--- src/models/randomforest_model.py
+++ src/models/randomforest_model.py
@@ -5,8 +5,8 @@ from src.models.base_model import BaseModel
 
 
 class RandomForestModel(BaseModel):
-    def __init__(self):
-        self.model = RandomForestClassifier()
+    def __init__(self, random_state=None):
+        self.model = RandomForestClassifier(class_weight='balanced', max_depth=15, n_estimators=200, random_state=random_state)
 
     def train(self, X, y):
         self.model.fit(X, y)
@@ -14,7 +14,7 @@ class RandomForestModel(BaseModel):
     def predict(self, X):
         return self.model.predict(X)
 
-    def save(self, path):
+    def export(self, path):
         joblib.dump(self.model, path)
 
     def load(self, path):
