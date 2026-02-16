9f6f8e21b394c10808fdf0c86d0482c5b51186a0
Samir Shrestha <theonlysamir@gmail.com>
2026-02-15 20:12:45 -0500

    better flexibility added

--- src/loader/load_dataset.py
+++ src/loader/load_dataset.py
@@ -4,13 +4,12 @@ from tqdm import tqdm
 from src.features.BearingData import BearingData
 from src.features.SignalProcessor import SignalProcessor
 
-def load_all_matfiles(channels=None):
+def load_all_matfiles(root_dir="../dataset/paderborn-db",channels=None,signal_processor=SignalProcessor):
     """
-    Recursively load all .mat files in dataset and extract features.
+    Recursively load all .mat files in dataset and extract features and signals.
     """
-    root_dir = "../dataset/paderborn-db"
     all_data = []
-
+    skipped_file_count = 0
     for subdir, _, files in os.walk(root_dir):
         mat_files = [f for f in files if f.endswith('.mat')]
         for f in tqdm(mat_files, desc=f"Processing {subdir}"):
@@ -30,7 +29,7 @@ def load_all_matfiles(channels=None):
                 for ch_name, signal in bd.signals.items():
                     if channels is not None and ch_name not in channels:
                         continue
-                    sp = SignalProcessor(signal)
+                    sp = signal_processor(signal)
                     feats = sp.get_all_features(prefix=ch_name)
                     row.update(feats)
 
@@ -38,8 +37,9 @@ def load_all_matfiles(channels=None):
 
             except Exception as e:
                 print(f"Error loading {full_path}: {e}")
+                skipped_file_count += 1
                 continue
-
+    print(f"Skipped {skipped_file_count} files.")
     df = pd.DataFrame(all_data)
     return df
 
